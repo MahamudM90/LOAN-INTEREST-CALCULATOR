6f7156e65a25ab95bf0affabbd75eff83963cac6
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-01-31 23:21:52 +0600

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="footer bg-dark text-white mt-5 p-3 text-center">
+      Made with <i className="fas fa-heart" /> and <i className="fas fa-code" />{' '}
+      by{' '}
+      <a
+        href="https://mahamudportfolio.netlify.app/?fbclid=IwAR1Zdm-0yh7a4zVRejM7PTtADAFrDmqDduCVhNPqrOh55ktwr8BQiZt-rqk"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Mahamud Hasan
+      </a>
+    </footer>
+  );
+};
