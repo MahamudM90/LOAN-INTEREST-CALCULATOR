fa2855b82eb4e55207e13085493e9187056137f1
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2022-02-02 19:15:01 +0600

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <footer className="footer bg-dark text-white mt-5 p-3 text-center">
+    <footer className="footer bg-dark text-white mt-5 p-4 text-center">
       Made with <i className="fas fa-heart" /> and <i className="fas fa-code" />{' '}
       by{' '}
       <a
